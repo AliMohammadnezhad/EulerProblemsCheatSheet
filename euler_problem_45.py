13b324436983d7f5bd1c3a20ace01d401f65370f
AliMohammadnezhad <alimohammadnejad271@gmail.com>
2022-06-14 15:38:55 -0400

    Solve the Euler problem Triangular, pentagonal, and hexagonal

---
+++ euler_problem_45.py
@@ -0,0 +1,19 @@
+def triangle(n):
+    return n*(n+1)//2
+
+def pentagonal(n):
+    return n*(3*n-1)//2
+
+def hexagonal(n):
+    return n*(2*n-1)
+
+def main():
+    p = set(pentagonal(n) for n in range(100000))
+    h = set(hexagonal(n) for n in range(100000))
+    for n in range(100000):
+        t = triangle(n)
+        if t in p and t in h and t > 40755:
+            print(t)
+
+if __name__ == "__main__":
+    main()
